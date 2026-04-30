2b2dcf904eb4dcb99126dcbd65ea4c3f674f3d3a
chloemiyo-web <chloemiyo@gmail.com>
2026-04-30 01:18:14 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname),
+      "@/components": path.resolve(__dirname, "components"),
+      "@/app": path.resolve(__dirname, "app"),
+    };
     return config;
   },
 };
